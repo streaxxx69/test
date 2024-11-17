73505ccae5ede342dd4c6909ddc9127186e48e55
bitotio <glebushkapavl2009q@gmail.com>
2024-11-17 15:34:39 +0300

    add

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print('Hello')
+from tkinter import *
+w =800
+h =500
+player_size = 100
+x1, y1 = 50, 50
+player_color ='red'
+x_finish= w - 50
+def key_handler(event):
+    canvas.move(player_id, 10,0)
+
+window = Tk()
+window.title('догони меня если сможешь')
+canvas = Canvas(window, width=w,height=h,bg='white')
+canvas.pack()
+
+
+player_id = canvas.create_rectangle(x1, y1, x1 + player_size, y1 + player_size, fill=player_color)
+finish_id =canvas.create_rectangle(x_finish, 0, x_finish + 10, h, fill='black')
+
+window.bind('KeyPress', key_handler)
+window.mainloop()
