b407d322ea19287c4bb8d3d114bc9f69179d826b
bitotio <glebushkapavl2009q@gmail.com>
2024-11-17 15:24:37 +0300

    add

---
+++ main.py
@@ -0,0 +1,21 @@
+from tkinter import *
+w =
+h =
+player_size =
+x1, y1 =
+player_color =
+x_finish
+def key_handler(event):
+    pass
+
+window = TK()
+window.title('догони меня если сможешь')
+canvas = Canvas(window, width=w,height=h,bg='white')
+canvas_pack()
+
+
+player_id =
+finish_id =
+
+window.bind('KeyPress', key_handler)
+window.mainloop
